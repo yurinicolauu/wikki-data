b0aae4ab22590a7916b325aaa45186d68880839e
Yuri Nicolau <ncl.yuri@gmail.com>
2024-04-23 20:32:19 -0300

    responsivity created

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 window.onscroll = function() {
-  const navbar = document.querySelector('#Header');
+  const navbar = document.querySelector('#Header')
   if (window.scrollY > 50) { // Altere "50" para a distância de scroll desejada
-      navbar.classList.add('scrolled');
+      navbar.classList.add('scrolled')
   } else {
-      navbar.classList.remove('scrolled');
+      navbar.classList.remove('scrolled')
   }
 };
 
 document.getElementById('toggle-mode').addEventListener('click', function() {
-  document.body.classList.toggle('dark');
+  document.body.classList.toggle('dark')
+});
+
+document.getElementById('toggle-mode').addEventListener('dblclick', function(event) {
+  event.preventDefault();
 });
