7ae37a03e8db9101cba6771ac2e3e8e00aa680bf
Yuri Nicolau <ncl.yuri@gmail.com>
2024-04-22 23:53:42 -0300

    toggle dark-mode created

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+  const navbar = document.querySelector('#Header');
+  if (window.scrollY > 50) { // Altere "50" para a distância de scroll desejada
+      navbar.classList.add('scrolled');
+  } else {
+      navbar.classList.remove('scrolled');
+  }
+};
+
+document.getElementById('toggle-mode').addEventListener('click', function() {
+  document.body.classList.toggle('dark');
+});
